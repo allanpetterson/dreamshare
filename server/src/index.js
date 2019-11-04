be58ebeffa877f83076453f51e3906e13dfecbd1
Allan Petterson <allan.pinheiro@agilecontent.com>
2019-11-04 14:50:56 -0300

    Server cards

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const routes = require('./routes');
+const models = require('./models');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    req.context = {
+        models,
+    };
+    next();
+});
+
+// Routes
+app.use('/cards', routes.cards);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ server/src/models/index.js
@@ -0,0 +1,22 @@
+const cards = [
+    {
+        id: 1,
+        title: 'Sed leo enim, condimentum',
+        desc: 'Quisque libero libero, dictum non turpis in, luctus semper lorem. Donec rhoncus a leo sit amet facilisis',
+        image: 'https://images.unsplash.com/photo-1516542076529-1ea3854896f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=400'
+    },
+    {
+        id: 2,
+        title: 'Morbi velit risus',
+        desc: 'Praesent tellus tortor, facilisis ac nulla ac, rutrum posuere leo. Curabitur a lacinia nisl.',
+        image: 'https://images.unsplash.com/photo-1445810694374-0a94739e4a03?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=400'
+    },
+    {
+        id: 3,
+        title: 'Sed leo enim, condimentum',
+        desc: 'Nunc accumsan nulla nec quam fringilla, et cursus ipsum condimentum. Nullam et mauris a orci mattis pellentesque.',
+        image: 'https://images.unsplash.com/photo-1519338381761-c7523edc1f46?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=400'
+    }
+]
+
+module.exports = { cards };
